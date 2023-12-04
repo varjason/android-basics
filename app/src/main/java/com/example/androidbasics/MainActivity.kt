1b278fac1abe93bd788a0ac5860f175b7c111839
JaSoN Ng <jason0611@JaSoNs-MacBook-Pro.local>
2023-12-04 23:44:10 +0800

    third commit

--- app/src/main/java/com/example/androidbasics/MainActivity.kt
+++ app/src/main/java/com/example/androidbasics/MainActivity.kt
@@ -24,6 +24,7 @@ class MainActivity : ComponentActivity() {
                 ) {
                     Greeting("Android")
                     Greeting("Studio")
+                    Greeting("1")
                 }
             }
         }
